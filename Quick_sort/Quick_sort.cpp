97ddd54bcd49622357ffea6b421857311e1b084d
iffa28 <127277497+iffa28@users.noreply.github.com>
2023-03-31 20:42:01 +0700

    number of data movement

--- Quick_sort/Quick_sort.cpp
+++ Quick_sort/Quick_sort.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int arr[20]; //Array of integers to hold values 
 int cmp_count = 0;  // number of comparasion 
-int mov_count = 0; //number of data movement 
+int mov_count = 0; //number of data movement  
 int n;
 
 void input() {
