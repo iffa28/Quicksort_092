ae0cc3179eb4b275493514abe034d942712a5732
iffa28 <127277497+iffa28@users.noreply.github.com>
2023-03-31 20:40:55 +0700

    Array of integers to hold values

--- Quick_sort/Quick_sort.cpp
+++ Quick_sort/Quick_sort.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int arr[20]; //Array of integers to hold values
+int arr[20]; //Array of integers to hold values 
 int cmp_count = 0;  // number of comparasion
 int mov_count = 0; //number of data movement 
 int n;
